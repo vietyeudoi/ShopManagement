a1a3ec66d850b547aaf2b06aacd80d8b72b0ec3b
thao-wq <thaod3797@gmail.com>
2026-01-03 18:36:17 +0700

    admin controller

---
+++ Sieu Thi Mini/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sieu_Thi_Mini.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sieu Thi Mini/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sieu_Thi_Mini.Models;
+
+namespace Sieu_Thi_Mini.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ShopManagementContext _context;
+        public UserController(ShopManagementContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+        public IActionResult Details(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+        // GET: Admin/User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: Admin/User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        // GET: Admin/User/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+        // POST: Admin/User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, User user)
+        {
+            if (id != user.UserId)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(user);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+        // GET: Admin/User/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+        // POST: Admin/User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
